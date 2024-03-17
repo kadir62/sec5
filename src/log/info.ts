d371ccbddf0830a5bc26f11591bcb609aea88843
Kadir <k4dircelikk@gmail.com>
2024-03-17 17:17:35 +0300

    feat: info log

--- src/log/info.ts
+++ src/log/info.ts
@@ -1,8 +1,8 @@
 import chalk from 'chalk'
 
-export default function ok(
+export default function info(
   message: string,
   from: `${string}` = 'SEC5'
 ): void {
-  console.log(chalk.blue(`[${from} OK] ${message}`))
+  console.log(chalk.blue(`[${from} INFO] ${message}`))
 }
