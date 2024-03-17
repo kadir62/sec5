64458138a4f0b2e9920dbb9ffc15c3acffebd35e
Kadir <k4dircelikk@gmail.com>
2024-03-17 20:19:21 +0300

    feat: version check

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import todoCheck from './todoCheck'
 import compareVersions from './changelog'
+import versionCheck from './versionCheck'
 
 todoCheck()
 compareVersions()
+versionCheck()
---
+++ src/versionCheck.ts
@@ -0,0 +1,36 @@
+import error from './log/error'
+import warn from './log/warn'
+import info from './log/info'
+import resolveConfig from './config/resolveConfig'
+import fileToPath from './utils/fileToPath'
+
+const packageJson = (
+  await import(fileToPath('package.json'), {
+    assert: {
+      type: 'json'
+    }
+  })
+).default
+
+const config = resolveConfig()
+
+const allowNoVersion = config?.allowNoVersion
+
+export default function versionCheck() {
+  if (packageJson.version === null || packageJson.version === undefined) {
+    if (allowNoVersion === true) {
+      warn(
+        `Package do not have any version. package.json: \n\r${fileToPath(
+          'package.json'
+        )}`
+      )
+    } else {
+      error(
+        `Package do not have any version. package.json: \n\r${fileToPath(
+          'package.json'
+        )}`
+      )
+      process.exit(1)
+    }
+  }
+}
