5e195cb191a1ea102b29c8a59bb2cafbfccdd3e1
Kadir <k4dircelikk@gmail.com>
2024-03-18 17:23:59 +0300

    feat: prettier check for formating

---
+++ src/prettier.ts
@@ -0,0 +1,28 @@
+import { exec } from 'child_process'
+import warn from './log/warn'
+import info from './log/info'
+import error from './log/error'
+import resolveConfig from './config/resolveConfig'
+
+const command = 'prettier --check .'
+
+const config = resolveConfig()
+
+const allowNoFormat = config?.allowNoFormat
+
+export default function prettierCheck() {
+  exec(command, (err, _, stderr) => {
+    if (err && stderr.toLowerCase().includes('run prettier to fix.')) {
+      if (allowNoFormat == true) {
+        warn('Codes are not formatted correctly')
+      } else {
+        error('Codes are not formatted correctly')
+      }
+    } else if (err) {
+      error('An error occurred during Prettier check. Error:')
+      console.error('\t' + stderr)
+    } else {
+      info('Codes are formatted correctly with Prettier')
+    }
+  })
+}
