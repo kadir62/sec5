0e130dfd18204bb5d2ff1c76c3449721b6a4f0bf
Kadir <k4dircelikk@gmail.com>
2024-03-18 17:24:33 +0300

    style: prettier

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import todoCheck from './todoCheck'
 import compareVersions from './changelog'
 import versionCheck from './versionCheck'
+import prettierCheck from './prettier'
 
 todoCheck()
 compareVersions()
 versionCheck()
+prettierCheck()
