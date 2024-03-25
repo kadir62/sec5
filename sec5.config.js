2d0be96917df97cf155928f5705e7c474f619a73
Kadir <k4dircelikk@gmail.com>
2024-03-25 22:15:59 +0300

    chore: new config

--- sec5.config.js
+++ sec5.config.js
@@ -1,6 +1,8 @@
 export default {
-  allowComments: false,
   allowNonWritedChangelog: false,
+  allowComments: false,
   allowNoVersion: false,
-  allowNoFormat: false
+  allowNoFormat: false,
+  allowNoLint: false,
+  ignoredChecks: []
 }
