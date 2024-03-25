658b8cb93136b737d2a351819a7490c9df7b9f30
Kadir <k4dircelikk@gmail.com>
2024-03-25 21:39:32 +0300

    chore: allowComments: false

--- sec5.config.js
+++ sec5.config.js
@@ -1,9 +1,5 @@
 export default {
-  /**
-   * Allowed beacuse of `src/check.ts` and build (`bin/sec5.js`) includes strings like FIXME and more.
-   * The number of comments should not exceed 2.
-   */
-  allowComments: true,
+  allowComments: false,
   allowNonWritedChangelog: false,
   allowNoVersion: false,
   allowNoFormat: false
